a24a3b7fa88f7a5d40d32f0898619421cf0e6976
wangdaoo <wangdaoo@yeah.net>
2021-07-24 11:27:11 +0800

    feat(Array): 数组差集处理 :tada:

--- 1.判断数组中的重复元素.js
+++ 1.判断数组中的重复元素.js
@@ -16,7 +16,7 @@ let containsDuplicateBad = function (nums) {
   }
   return false;
 };
-console.log(containsDuplicateBad(arr));
+console.log(containsDuplicateBad(arr)); // false
 
 /**
  * Set 对象是值的集合。您可以按插入顺序遍历集合的元素。集合中的值只能出现一次；它在Set的集合中是唯一的。
@@ -27,14 +27,14 @@ console.log(containsDuplicateBad(arr));
 let containsDuplicateGood = function (nums) {
   return new Set(nums).length !== nums.length;
 };
-console.log(containsDuplicateBad(arr));
+console.log(containsDuplicateBad(arr)); // false
 
 /**
- * 查看两个数组中是否有重复交集
+ * Set 查看两个数组中是否有重复交集
  * bool 为空数组则无交集
  * 不为空则有交集, 数组中包含该交集值
  */
-const a = [1, 2, 3, 4];
+const a = [1, 2, 3, 4, 7];
 const b = [4, 5, 6, 7];
 function insertion(a, b) {
   a = new Set(a);
@@ -44,4 +44,29 @@ function insertion(a, b) {
   });
   return bool;
 }
-console.log(insertion(a, b));
+console.log(insertion(a, b)); // [4, 7]
+
+/** 
+ * Set 查看数据中的并集、去重
+ */
+function union(a, b) {
+  const newArr = [...new Set([...a, ...b])]
+  console.log(newArr);  // [ 1, 2, 3, 4, 7, 5, 6 ]
+}
+union(a, b)
+
+/** 
+ * a 与 b 的差集
+ */
+function difference(a, b) {
+  a = new Set(a);
+  b = new Set(b);
+  const bool = [...a].filter((item) => {
+    return !b.has(item);
+  });
+  return bool;
+}
+console.log(difference(a, b));  // [ 1, 2, 3 ]
+
+var d = a.filter(function(v){ return b.indexOf(v) == -1 })
+console.log(d); // [ 1, 2, 3 ]
