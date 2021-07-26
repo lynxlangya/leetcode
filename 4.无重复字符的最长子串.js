903673bbc5f64499546dcc1ae84c2d4752f83917
wangdaoo <wangdaoo@yeah.net>
2021-07-26 09:58:56 +0800

    feat(string): 无重复字符的最长子串 :tada:

---
+++ 4.无重复字符的最长子串.js
@@ -0,0 +1,35 @@
+/**
+ * 给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度。
+ */
+
+const s = 'ababcda';
+function lengthOfLongestSubstring(str) {
+  // let min = 0;
+  // let ans = 0;
+  // for (let i = 0; i < str.length; i++) {
+  //   console.log(str.indexOf(str[i]), min);
+  //   if(str.indexOf(str[i], min) < i) {
+  //     // console.log(str);
+  //     min = str.indexOf(str[i], min) + 1
+  //   } else {
+  //     ans = Math.max(ans, i - min + 1)
+  //     // console.log(ans);
+  //   }
+  // }
+
+  let arr = [];
+  let max = 0;
+  for (let i = 0; i < str.length; i++) {
+    let index = arr.indexOf(str[i]);
+    if (index !== -1) {
+      arr.splice(0, index + 1);
+    }
+    // console.log(str.charAt(i));
+    arr.push(str.charAt(i));
+    console.log(arr);
+    max = Math.max(arr.length, max);
+  }
+  console.log(max);
+}
+
+lengthOfLongestSubstring(s);
