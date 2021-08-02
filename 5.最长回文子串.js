6a3bb34880326bb7ce75e9cd9efdc66d4f4c01c9
wangdaoo <wangdaoo@yeah.net>
2021-08-02 12:55:44 +0800

    feat(animation): requestAnimationFrame test :rockert:

--- 5.最长回文子串.js
+++ 5.最长回文子串.js
@@ -11,6 +11,33 @@
  */
 var longestPalindrome = function (s) {
   console.log(s);
+  /** 长度为1，返回自身 */
+  if (s.length === 1) return s;
+
+  let arr = [];
+  for (let i = 0; i < s.length; i++) {
+    arr[i] = [];
+  }
+  console.log(arr);
+
+  let begin = 0;
+  let max = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    let j = 0;
+    while (j <= i) {
+      if (s[j] == s[i] && (i - j <= 1 || arr[j + 1][i - 1])) {
+        arr[j][i] = 1;
+        if (i - j + 1 > max) {
+          begin = j;
+          max = i - j + 1;
+        }
+      }
+      j++;
+    }
+  }
+  console.log(s.substr(begin, max));
+  return s.substr(begin, max)
 };
 // @lc code=end
-longestPalindrome('ababa');
+longestPalindrome('abcdae');
