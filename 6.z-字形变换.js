ff6bb4d6879ec5703afd3007174b524737cc24b2
wangdaoo <wangdaoo@yeah.net>
2021-08-16 20:39:31 +0800

    feat: z-字形变换 :tada:

--- 6.z-字形变换.js
+++ 6.z-字形变换.js
@@ -10,8 +10,16 @@
  * @param {number} numRows
  * @return {string}
  */
-var convert = function(s, numRows) {
-
+var convert = function (s, numRows) {
+  if (numRows === 1) return s;
+  const rows = new Array(numRows).fill('');
+  const period = 2 * numRows - 2;
+  for (let i = 0; i < s.length; i++) {
+    rows[Math.min(i % period, period - (i % period))] += s[i];
+  }
+  console.log(rows);
+  console.log(rows.join(''));
+  return rows.join('');
 };
+convert('HELLO_WORLD', 4);
 // @lc code=end
-
