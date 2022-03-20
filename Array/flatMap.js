c44633da6b292ae037f20908a9ef8c139120e4c5
山人自有妙计 <wangdaoo@yeah.net>
2022-03-20 14:27:06 +0800

    feat: toString 打平数组 :rocket:

--- Array/flatMap.js
+++ Array/flatMap.js
@@ -1,11 +1,17 @@
 const list = [1, 2, 3, 4, [5, 6, [7, 8, [9, 10]]]];
 
-console.log(list.flat());
+console.log(list.flat(Infinity)); // 0.059s
+// console.log(list.toString());
+
+/**
+ * 利用toSting()方法，将数组转换为字符串，然后再转换为数组。
+ */
+console.log(list.toString().split(','));    // 0.063s
 
 const words = ['Hello World', 'Hello JS'];
 
-/** 
+/**
  * flatMap: 返回一个新的数组，其中包含了原数组中的所有元素，并且把原数组中的元素展开为多个元素。
- * 
+ *
  */
 console.log(words.flatMap((word) => word.split(' '))); // [ 'Hello', 'World', 'Hello', 'JS' ]
