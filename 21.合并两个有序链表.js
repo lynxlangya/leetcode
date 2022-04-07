b36a9aa61c7b9dc4cc3020c2a7acbe9dacc1c4ea
山人自有妙计 <wangdaoo@yeah.net>
2022-04-07 10:00:56 +0800

    feat: mergeTwoLists :rocket:

--- 21.合并两个有序链表.js
+++ 21.合并两个有序链表.js
@@ -5,13 +5,15 @@
  */
 
 // @lc code=start
-/**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
+
+// Definition for singly-linked list.
+class ListNode {
+    constructor(val, next) {
+        this.val = (val === undefined ? 0 : val);
+        this.next = (next === undefined ? null : next);
+    }
+}
+
 /**
  * @param {ListNode} list1
  * @param {ListNode} list2
@@ -21,6 +23,8 @@ var mergeTwoLists = function(list1, list2) {
     if (!list1) return list2;
     if (!list2) return list1;
     let head = new ListNode();
+    console.log(head);
+    console.log(list1, list2);
     let cur = head;
     while (list1 && list2) {
         if (list1.val < list2.val) {
@@ -34,8 +38,10 @@ var mergeTwoLists = function(list1, list2) {
     }
     if (list1) cur.next = list1;
     if (list2) cur.next = list2;
+    console.log(head);
     return head.next;
 };
-console.log(mergeTwoLists([1,2,4],[1,3,4]));
+mergeTwoLists([1,2,4],[1,3,4])
+// console.log('merge', mergeTwoLists([1,2,4],[1,3,4]));
 // @lc code=end
 
