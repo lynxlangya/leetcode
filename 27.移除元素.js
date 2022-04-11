0b18af8ea077a86e835a39e83cc4a730b8ac2e00
山人自有妙计 <wangdaoo@yeah.net>
2022-04-11 11:06:20 +0800

    feat: 移除元素 :rocket:

--- 27.移除元素.js
+++ 27.移除元素.js
@@ -9,13 +9,15 @@
  * @param {number[]} nums
  * @param {number} val
  * @return {number}
+ * 原地算法：https://zh.wikipedia.org/wiki/%E5%8E%9F%E5%9C%B0%E7%AE%97%E6%B3%95
+ * 思路：
+ * 1. 循环遍历数组，如果当前元素不等于val, 则将当前元素放到i位置，i++
+ * 2. 如果当前元素等于val, 则i不变，j++
+ * 3. 循环结束，j的值就是移除的元素的个数，i的值就是需要保留的元素的个数
  */
 var removeElement = function (nums, val) {
     let i = 0;
     let j = 0;
-    // 循环遍历数组，如果当前元素不等于val，则将当前元素放到i的位置，并且i++
-    // 如果当前元素等于val，则j++
-    // 循环结束后，j的值就是需要移除的元素的个数。i的值就是需要保留的元素的个数
     while (j < nums.length) {
         if (nums[j] !== val) {
             nums[i] = nums[j];
